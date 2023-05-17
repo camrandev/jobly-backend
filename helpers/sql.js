96f21eb99dbc3189885890870ed13e7fdd021024
Ashley Lin <ashleyylin11@gmail.com>
2023-05-17 11:11:55 -0700

    start sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,13 +2,23 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** sqlForPartialUpdate prepares JSON data submitted by user to be used
+ * in SQL queries to update a part of a record in the database.
+ *
+ * It takes as input some JSON, for example: {firstName: 'Aliya', age: 32}
+ *
+ * It returns an object with two properties. setCols is a string, and values
+ * is an array. It looks like:
+  * {
+      setCols: '"first_name"=$1', '"age"=$2',
+      values: ['Aliya', 32],
+    }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
---
+++ helpers/sql.test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+const DATA_TO_UPDATE = {firstName: 'Aliya', age: 32}
+const JS_TO_SQL = {
+  firstName: "first_name",
+}
+
+describe("sqlForPartialUpdate", function () {
+  test("works: returns expected data", function () {
+    const result = sqlForPartialUpdate(DATA_TO_UPDATE, JS_TO_SQL)
+    expect(result).toEqual({
+      setCols: '"first_name"=$1, "age"=$2',
+      values: ['Aliya', 32],
+    });
+  });
+
+  test("no JSON data: throws 400 BadRequestError", function () {
+    const result = sqlForPartialUpdate({}, JS_TO_SQL)
+    expect(result).toThrow(BadRequestError);
+  });
+
+  // test("works: default no admin", function () {
+  //   // given the security risk if this didn't work, checking this specifically
+  //   const token = createToken({ username: "test" });
+  //   const payload = jwt.verify(token, SECRET_KEY);
+  //   expect(payload).toEqual({
+  //     iat: expect.any(Number),
+  //     username: "test",
+  //     isAdmin: false,
+  //   });
+  // });
+});
